160c58b5d764b2ae8c2bbcd1ab4247e7158c09bd
Yikai Zhou <zhou.yikai76@gmail.com>
2025-02-09 18:01:10 -0500

    adding tailwind great success

--- webview/src/App.tsx
+++ webview/src/App.tsx
@@ -2,10 +2,7 @@ function App() {
   return (
     <div className="bg-gradient-to-r from-blue-600 to-purple-500 p-10">
       <p className="text-white/80 text-xl font-semibold">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, explicabo
-        doloremque deserunt, voluptates, fugiat dolorem consectetur odio autem
-        quas ipsa veniam ducimus necessitatibus exercitationem numquam assumenda
-        natus beatae sed velit!
+        Hello this is sad
       </p>
     </div>
   );
